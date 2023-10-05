f10afb87b103056bc0581bcf179b0281bab524d9
Tanya Golubev <44844712+golubevtanya@users.noreply.github.com>
2023-10-05 10:44:13 +0200

    Update test2.py

--- src/test2.py
+++ src/test2.py
@@ -20,7 +20,7 @@ def main():
     for line in sys.stdin:
         if not all([_ in line for _ in key_words]):
             continue
-        year = re.search(reg_year, line).group(1)
+        year = re.search(reg_year,line).group(1)
         if year not in years:
             continue
         tags_str = re.search(reg_tags,line)
