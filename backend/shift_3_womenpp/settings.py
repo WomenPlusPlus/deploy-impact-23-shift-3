99ee2b9ed24be97fb3776f6124f5a8ee0e9bd084
Tanya Golubev <44844712+golubevtanya@users.noreply.github.com>
2023-10-05 14:54:34 +0200

    Update settings.py

--- backend/shift_3_womenpp/settings.py
+++ backend/shift_3_womenpp/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'shift_3_womenpp.urls'
+ROOT_URLCONF='shift_3_womenpp.urls'
 
 TEMPLATES = [
     {
