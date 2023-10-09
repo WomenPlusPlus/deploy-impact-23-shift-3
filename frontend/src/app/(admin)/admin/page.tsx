741f28e40e4b2d58e4a20ffdaf63671736f59ed6
Janice Shaw <hello@janiceshaw.co.uk>
2023-10-09 21:00:37 +0200

    admin added

---
+++ frontend/src/app/(admin)/admin/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+
+import { redirect } from "next/navigation";
+
+export default function HomePage() {
+  // TODO: temp redirect until we have something to display here
+  // redirect("/signin");
+  return (
+    <Box sx={{ display: "flex", marginTop: "80px" }}>
+      <div>
+        <Typography variant="h1">
+          Admin Dashboard -- - need content --
+        </Typography>
+      </div>
+    </Box>
+  );
+}
