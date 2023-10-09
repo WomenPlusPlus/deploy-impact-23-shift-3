a79d8bea89715dfe91c21c1fde738bcecf694354
Martina Babinska <martina.babinska.sk@gmail.com>
2023-10-09 23:15:28 +0200

    move candidate profila file set TOKEN to local storage

--- frontend/src/app/(site)/candidate/profile/page.tsx
+++ frontend/src/app/(site)/candidate/profile/page.tsx
@@ -1,29 +1,73 @@
-import * as React from 'react';
-import Container from '@mui/material/Container';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
+"use client";
+
+import * as React from "react";
+import { useContext } from "react";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import { SignInProviderContext } from "@/components/providers/SignInProvider";
+import { useQuery } from "@tanstack/react-query";
+import Divider from "@mui/material/Divider";
+import { Avatar, FormControl } from "@mui/material";
+import TextField from "@mui/material/TextField";
+import Card from "@mui/material/Card";
+import { fetchCandidate } from "@/lib/fetch-candidate";
 
 export default function ProfilePage() {
+  // useContext is a Hook in react. It is a way to pass data through the component tree without having to pass props down manually at every level.
+  const signInContext = useContext(SignInProviderContext);
+  // useQuery is a Hook provided by the React Query library, and it is used for fetching and managing data in React applications.
+  // firstParam: key
+  //secondParam: The function that performs the data-fetching operation. This function should return a Promise that resolves to the data you want to retrieve
+  //thirdParam: optional configuration object that allows you to customize the behavior of the query. (for more info read doc)
+  const { data } = useQuery(
+    ["candidates", signInContext.auth?.user?.id],
+    () => fetchCandidate(signInContext.auth?.user?.id),
+    {
+      enabled: !!signInContext.auth?.user?.id,
+    },
+  );
+
   return (
     <Container>
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-            <Typography variant="h3" component="h1" gutterBottom sx={{mt:5}}>
-               Candidate - Profile page
-              </Typography>
-              <Typography variant="h5" component="h2" gutterBottom>
-                this is where the candidate can update there profile
-              </Typography>
-              <Typography variant="body1">
-               Martina move page here please 
-              </Typography>
-      </Box>
+      <Card sx={{ padding: "8px" }}>
+        <Box sx={{ display: "flex", flexDirection: "column", gap: "8px" }}>
+          <Box>
+            <Typography variant="h6">BASIC DETAILS</Typography>
+          </Box>
+          <Divider />
+          <Box
+            sx={{ display: "flex", gap: "56px" }}
+            paddingX={"40px"}
+            paddingY={"8px"}
+          >
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <Avatar
+                alt="Avatar"
+                src="images/avatar.png"
+                sx={{ width: 120, height: 120 }}
+              />
+            </Box>
+            <Box sx={{ display: "flex", flexGrow: 1 }}>
+              <FormControl fullWidth variant="standard">
+                <TextField
+                  id={"email"}
+                  size={"small"}
+                  value={signInContext.auth?.user?.email || ""}
+                  variant={"standard"}
+                  label={"Email"}
+                />
+              </FormControl>
+            </Box>
+          </Box>
+        </Box>
+      </Card>
     </Container>
   );
 }
--- frontend/src/components/providers/SignInProvider.tsx
+++ frontend/src/components/providers/SignInProvider.tsx
@@ -1,5 +1,12 @@
 "use client";
-import { createContext, FC, PropsWithChildren, useState } from "react";
+import {
+  createContext,
+  FC,
+  PropsWithChildren,
+  useEffect,
+  useState,
+} from "react";
+import { CircularProgress, Stack } from "@mui/material";
 
 export type Authentication = {
   authenticated: boolean;
@@ -10,20 +17,51 @@ export type Authentication = {
 type Context = {
   auth: Authentication;
   setAuth: (auth: Authentication) => void;
+  signOut: () => void;
 };
 export const SignInProviderContext = createContext<Context>(
   {} as unknown as Context,
 );
-
+const LOCAL_STORAGE_KEY = "womenPlusPlusAuth";
 export const SignInProvider: FC<PropsWithChildren> = ({ children }) => {
+  const [isInitialized, setIsInitialized] = useState(false);
   const [auth, setAuth] = useState<Authentication>({
     authenticated: false,
     access_token: "",
     user: null,
   });
+
+  const handleAuth = (auth: Authentication) => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(auth));
+    setAuth(auth);
+  };
+  useEffect(() => {
+    const auth = localStorage.getItem(LOCAL_STORAGE_KEY);
+    if (auth) {
+      setAuth(JSON.parse(auth));
+    }
+    setIsInitialized(true);
+  }, []);
+  const handleSignOut = () => {
+    setAuth({ authenticated: false, access_token: "", user: null });
+    localStorage.removeItem(LOCAL_STORAGE_KEY);
+  };
   return (
-    <SignInProviderContext.Provider value={{ auth, setAuth }}>
-      {children}
+    <SignInProviderContext.Provider
+      value={{ auth, setAuth: handleAuth, signOut: handleSignOut }}
+    >
+      {isInitialized ? (
+        children
+      ) : (
+        <Stack
+          width={"100vw"}
+          height={"100vh"}
+          alignItems={"center"}
+          justifyContent={"center"}
+        >
+          <CircularProgress />
+        </Stack>
+      )}
     </SignInProviderContext.Provider>
   );
 };
