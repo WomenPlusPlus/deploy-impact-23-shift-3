6a8a5211ebd045dac2cabd66c1d4215db4e0883c
Janice Shaw <hello@janiceshaw.co.uk>
2023-10-10 13:18:36 +0200

    added icons to submenu links

--- frontend/src/components/site/header.tsx
+++ frontend/src/components/site/header.tsx
@@ -8,6 +8,7 @@ import Typography from "@mui/material/Typography"
 import Button from "@mui/material/Button"
 import IconButton from "@mui/material/IconButton"
 import SearchIcon from "@mui/icons-material/Search"
+import Paper from "@mui/material/Paper"
 import Link from "next/link"
 
 
@@ -19,24 +20,28 @@ interface HeaderProps {
 export default function Header(props: HeaderProps) {
   const {title } = props
 
-  const DRAWER_WIDTH = 240
-
   return (
-
+<Paper elevation={3} sx={{backgroundColor:'#FCF8F4'}}>
       <Toolbar
         sx={{
-          borderBottom: 1,
+          // borderBottom: 1,
           borderColor: "divider",
-          // backgroundColor: "#14366F",
-         
+          // backgroundColor: "#14366F"
+        
+        
         }}
       >
+        
         <Button size="small">SHIFT</Button>
 
         <Button variant="outlined" size="small">
           <Link href="/signin" > Sign out</Link>
         
         </Button>
+      
       </Toolbar>
+      </Paper>
+
+
   )
 }
--- frontend/src/components/site/subHeader.tsx
+++ frontend/src/components/site/subHeader.tsx
@@ -8,11 +8,19 @@ import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import Link from "@mui/material/Link";
+import Container from "@mui/material/Container";
+
+import HomeIcon from "@mui/icons-material/Home";
+import WorkIcon from "@mui/icons-material/Work";
+import BusinessIcon from "@mui/icons-material/Business";
+import PersonIcon from "@mui/icons-material/Person";
+import SettingsIcon from "@mui/icons-material/Settings";
 
 interface HeaderProps {
   sections: ReadonlyArray<{
     title: string;
     url: string;
+    icon: string;
   }>;
 }
 
@@ -20,30 +28,55 @@ export default function SubHeader(props: HeaderProps) {
   const { sections, title } = props;
   //TODO: add title to props so can use it to determine current page
   return (
-    <Toolbar
-      component="nav"
-      variant="dense"
-      sx={{
-        justifyContent: "space-between",
-        overflowX: "auto",
-        maxWidth: "80%",
-        borderBottom: 1,
-        borderColor: "divider",
-        mx: "auto",
-      }}
-    >
-      {sections.map((section) => (
-        <Link
-          color="inherit"
-          key={section.title}
-          variant="body2"
-          underline="none"
-          href={section.url}
-          sx={{ mx: 3, display: "inline-block", maxWidth: "50%" }}
-        >
-          {section.title}
-        </Link>
-      ))}
-    </Toolbar>
+    <Container>
+      <Toolbar
+        component="nav"
+        variant="dense"
+        sx={{
+          justifyContent: "space-between",
+          borderBottom: 2,
+          borderColor: "divider",
+          mt: 3,
+        }}
+      >
+        {sections.map((section) => (
+          <Link
+            className="selected"
+            color="inherit"
+            key={section.title}
+            variant="body2"
+            underline="none"
+            href={section.url}
+            sx={{ mx: 3, mb: 2, display: "inline-block", textAlign: "center" }}
+          >
+            <Container>
+              {section.icon === "home" ? (
+                <HomeIcon color="inherit" sx={{ display: "inline-block" }} />
+              ) : null}
+              {section.icon === "jobs" ? (
+                <WorkIcon color="inherit" sx={{ display: "inline-block" }} />
+              ) : null}
+              {section.icon === "company" ? (
+                <BusinessIcon
+                  color="inherit"
+                  sx={{ display: "inline-block" }}
+                />
+              ) : null}
+              {section.icon === "profile" ? (
+                <PersonIcon color="inherit" sx={{ display: "inline-block" }} />
+              ) : null}
+              {section.icon === "settings" ? (
+                <SettingsIcon
+                  color="inherit"
+                  sx={{ display: "inline-block" }}
+                />
+              ) : null}
+            </Container>
+
+            {section.title}
+          </Link>
+        ))}
+      </Toolbar>
+    </Container>
   );
 }
