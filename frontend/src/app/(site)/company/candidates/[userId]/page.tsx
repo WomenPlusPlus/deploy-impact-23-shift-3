cf150852f57f13b75944065be4ee8293d1fe597b
Janice Shaw <hello@janiceshaw.co.uk>
2023-10-23 14:57:00 +0200

    set up param for when we have a userid

--- frontend/src/app/(site)/company/candidates/[userId]/page.tsx
+++ frontend/src/app/(site)/company/candidates/[userId]/page.tsx
@@ -2,16 +2,26 @@
 import Link from "@mui/material/Link";
 import Button from "@mui/material/Button";
 import Grid from "@mui/material/Grid";
+import { Suspense } from "react";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import Typography from "@mui/material/Typography";
 
 import ProfilePreview from "@/components/site/candidateProfile/profileView";
 
-export default function PreviewPage() {
+type Params = {
+  params: {
+    userId: string;
+  };
+};
+
+export default function UserProfilePage({params:{userId}}:Params) {
+
+ //Todo: use userId to get the user/candidate details
+  
   return (
     <>
       <Grid container sx={{ mb: 4 }}>
-        <Grid item md={6}></Grid>
+        <Grid item md={6}>for testing: {userId}</Grid>
         <Grid item md={6} sx={{ textAlign: "right" }}>
           <Link href="/company/candidates/">
             <Button
