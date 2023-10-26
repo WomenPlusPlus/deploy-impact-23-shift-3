7be31410bfcea81325d985ac3ec1be6db4af70d5
Guilherme Kruger <krugergui@gmail.com>
2023-10-26 04:30:44 +0200

    Removed Put from the available methods

--- backend/api/mixed_router.py
+++ backend/api/mixed_router.py
@@ -25,3 +25,15 @@ class APIRouter(routers.DefaultRouter):
         for singleView in self.singleViews:
             api_root_dict[singleView["route"]] = singleView["name"]
         return self.APIRootView.as_view(api_root_dict=api_root_dict)
+
+    def get_method_map(self, viewset, method_map):
+        """
+        Router class that disables the PUT method.
+        """
+
+        bound_methods = super().get_method_map(viewset, method_map)
+
+        if "put" in bound_methods.keys():
+            del bound_methods["put"]
+
+        return bound_methods
